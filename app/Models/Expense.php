9cff6abc911a6be4206d9dd6089935234f0e3bbb
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-07-15 21:30:45 +0200

    Add fillable and casts properties and relationships to category and users tables

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -3,8 +3,44 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'amount',
+        'description',
+        'date',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'date' => 'date',
+    ];
+
+    /**
+     * One-to-Many relationship with a User
+     *
+     * @return BelongsTo <p>
+     *     Many expenses can belong to one user
+     * </p>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * One-to-Many relationship with a Category
+     *
+     * @return BelongsTo <p>
+     *     Many expenses can belong to one category
+     * </p>
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
